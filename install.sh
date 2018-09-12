2a263bd15380d628179cecbe26acbb0f883e3b30
nebulak <nebulak@users.noreply.github.com>
2018-09-12 12:33:55 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+chmod +x ./pinentry-keepass
+sudo cp ./pinentry-keepass /usr/local/bin/pinentry-keepass
+
+# //TODO: update ~/.gnupg/gpg-agent.conf
+# pinentry-program /usr/local/bin/pinentry-keepass
